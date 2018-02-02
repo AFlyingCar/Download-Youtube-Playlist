d8ed0b25b1e5d95fba25f9b77176c61ac8a44bc4
Tyler Robbins <tyler@familyrobbins.com>
2018-02-01 16:13:26 -0800

    Updated.

--- dlYTPlaylist.sh
+++ dlYTPlaylist.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
 function invalidArgs(){
-    echo "Invalid number of arguments."
+    if [[ "$quiet" -eq 0 ]]; then
+        echo "Invalid number of arguments."
+    fi
     helpAndExit
     exit 1;
 }
 
 function helpAndExit(){
-    echo "dlYTPlaylist [OPTIONS]"
-    echo "    -h       Display this message and exit."
-    echo "    -m       Create a metadata file which stores the original URL."
-    echo "    -u [URL] The URL to use."
-    echo "    -d [DIR] The directory to save to."
+    if [[ "$quiet" -eq 0 ]]; then
+        echo "dlYTPlaylist [OPTIONS]"
+        echo "    -h       Display this message and exit."
+        echo "    -m       Create a metadata file which stores the original URL."
+        echo "    -u [URL] The URL to use."
+        echo "    -d [DIR] The directory to save to."
+        echo "    -q       Suppresses all output."
+    fi
+}
+
+function convert(){
+    if [[ `ls "$dlpath"/*."$1" -1 2>/dev/null | wc -l` != 0 ]]; then
+        if [[ "$quiet" -eq 0 ]]; then
+            echo "Converting all $1 files to mp3 format..."
+        fi
+
+        for i in "$dlpath"/*."$1"; do
+            newName=`echo "$i" | sed -e "s/$1/mp3/"` || { exit 1; };
+            ffmpeg `if [[ "$quiet" -eq 0 ]]; then echo "-loglevel quiet"; fi` -i \
+                "$i" -f mp3 "$newName" || { exit 1; };
+        done
+
+        rm "$dlpath"/*."$1" || { exit 1; };
+    fi
 }
 
 docreatemeta="0"
@@ -20,6 +41,7 @@ all_args=("$@")
 dlpath=`pwd`
 url="1"
 pstart=1
+quiet=0
 
 for ((index=0; index <= "$#"; index++)); do
     arg=${all_args[index]}
@@ -32,6 +54,8 @@ for ((index=0; index <= "$#"; index++)); do
         url="${all_args[++index]}"
     elif [[ "$arg" = "-d" ]]; then
         dlpath="${all_args[++index]}"
+    elif [[ "$arg" = "-q" ]]; then
+        quiet=1
     fi
 done
 
@@ -42,29 +66,27 @@ if [[ "$url" = "1" ]]; then
     then
         url=`cat "$dlpath"/META.info`
     else
-        echo "Unable to find a META.info file in $dlpath. Please create one with a valid URL in it or specify the URL."
+        if [[ "$quiet" -eq 0 ]]; then
+            echo "Unable to find a META.info file in $dlpath. Please create one with a valid URL in it or specify the URL."
+        fi
         invalidArgs
     fi
 fi
 
 if [[ "$docreatemeta"  = "1" ]]; then
-    echo "Writing Metadata file..."
+    if [[ "$quiet" -eq 0 ]]; then
+        echo "Writing Metadata file..."
+    fi
     touch "$dlpath"/META.info
     echo "$url" > "$dlpath"/META.info
 fi
 
 echo "Beginning download"
-youtube-dl -i -x --download-archive "$dlpath/archive.txt" -o "$dlpath/%(title)s-v=%(id)s.%(ext)s" "$url" # || { exit 1; };
+youtube-dl `if [[ "$quiet" -eq 1 ]]; then echo "-q"; fi` -i -x \
+                --download-archive "$dlpath/archive.txt" -o \
+                "$dlpath/%(title)s-v=%(id)s.%(ext)s" "$url" # || { exit 1; };
 
-if [[ `ls "$dlpath"/*.m4a -1 2>/dev/null | wc -l` != 0 ]]; then
-    echo "Converting all m4a files to mp3 format..."
-    for i in "$dlpath"/*.m4a; do
-        newName=`echo "$i" | sed -e 's/m4a/mp3/'` || { exit 1; };
-        ffmpeg -i "$i" -f mp3 "$newName" || { exit 1; };
-    done
-fi
 
-if [[ `ls "$dlpath"/*.m4a -1 2>/dev/null | wc -l` != 0 ]]; then
-    rm "$dlpath"/*.m4a || { exit 1; };
-fi
+convert m4a
+convert opus
 
