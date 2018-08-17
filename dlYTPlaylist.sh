3d6eeede87c9ca938fa7474945a3f8f0f609fa10
Tyler Robbins <tyler@familyrobbins.com>
2018-08-17 13:56:17 -0700

    Added options to generate an M3U playlist.

--- dlYTPlaylist.sh
+++ dlYTPlaylist.sh
@@ -16,6 +16,8 @@ function helpAndExit(){
         echo "    -u [URL] The URL to use."
         echo "    -d [DIR] The directory to save to."
         echo "    -q       Suppresses all output."
+        echo "    -p       Generate an m3u playlist file (default)."
+        echo "    -n       Does not generate an m3u playlist file."
     fi
 }
 
@@ -42,6 +44,7 @@ dlpath=`pwd`
 url="1"
 pstart=1
 quiet=0
+make_pl=1
 
 for ((index=0; index <= "$#"; index++)); do
     arg=${all_args[index]}
@@ -56,6 +59,10 @@ for ((index=0; index <= "$#"; index++)); do
         dlpath="${all_args[++index]}"
     elif [[ "$arg" = "-q" ]]; then
         quiet=1
+    elif [[ "$arg" = "-p" ]]; then
+        make_pl=1
+    elif [[ "$arg" = "-n" ]]; then
+        make_pl=0
     fi
 done
 
@@ -86,6 +93,10 @@ youtube-dl `if [[ "$quiet" -eq 1 ]]; then echo "-q"; fi` -i -x \
                 --download-archive "$dlpath/archive.txt" -o \
                 "$dlpath/%(title)s-v=%(id)s.%(ext)s" "$url" # || { exit 1; };
 
+if [[ $make_pl -eq 1 ]]; then
+    pl_name=$(basename $dlpath)
+    ls "$dlpath" | while read line; do echo "$dlpath/$line"; done > $dlpath/$pl_name.m3u
+fi
 
 convert m4a
 convert opus
