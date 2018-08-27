625f83e4332b7561ffc131ce6b82f9c5c0041b03
Tyler Robbins <tyler@familyrobbins.com>
2018-08-27 13:35:50 -0700

    [PS1] Fixed NoPlaylist command. Added conversion line from ogg to mp3. Added command to suppress download checks. Fixed syntax errors. Fixed conversion function.

--- dlYTPlaylist.ps1
+++ dlYTPlaylist.ps1
@@ -6,6 +6,7 @@
 # -d [DIR] -- Set save directory
 # -q       -- Suppress output
 # -n       -- Does not generate an m3u playlist file.
+
 Param(
     [Parameter(HelpMessage="Print help message and exit.")]
         [alias("h")]
@@ -24,7 +25,10 @@ Param(
         [switch]$Quiet=$false,
     [Parameter(HelpMessage="Does not generate an m3u playlist file.")]
         [alias("n")]
-        [switch]$NoPlaylist=$true
+        [switch]$NoPlaylist=$false,
+    [Parameter(HelpMessage="Do not download anything.")]
+        [alias("nd")]
+        [switch]$NoDownload=$false
 )
 
 function invalidArgs() {
@@ -39,12 +43,15 @@ function invalidArgs() {
 function helpMessage() {
     if(!$Quiet) {
         Write-Host "dlYTPlaylist [OPTIONS]"
-        Write-Host "    -h       Display this message and exit."
-        Write-Host "    -m       Create a metadata file which stores the original URL."
-        Write-Host "    -u [URL] The URL to use."
-        Write-Host "    -d [DIR] The directory to save to."
-        Write-Host "    -q       Suppresses all output."
-        Write-Host "    -n       Do not generate an m3u playlist file."
+        Write-Host "A powershell script which downloads and maintains playlists of music."
+        Write-Host "    -h,-Help              Display this message and exit."
+        Write-Host "    -m,-CreateMetaFile    Create a metadata file which stores the original URL."
+        Write-Host "    -u,-URL [URL]         The URL to use."
+        Write-Host "    -d,-Directory [DIR]   The directory to save to."
+        Write-Host "    -q,-Quiet             Suppresses all output."
+        Write-Host "    -n,-NoPlaylist        Do not generate an m3u playlist file."
+        Write-Host ""
+        Write-Host "https://github.com/AFlyingCar/Download-Youtube-Playlist"
     }
 }
 
@@ -53,16 +60,16 @@ function convert() {
         [Parameter(Mandatory=$true)][string]$Type
     )
 
-    files = Get-ChildItem $Directory -Filter *$Type ? { !$_.PSIsContainer }
+    $files = Get-ChildItem $Directory -Filter "*.$Type" | Where-Object { !$_.PSIsContainer }
 
     if(!$Quiet) {
         Write-Host "Converting all $Type files to mp3 format..."
     }
 
     foreach($i in $files) {
-        newname=($i -replace "$Type","mp3")
+        $newname = ($i -replace "$Type","mp3")
 
-        ffmpeg (&{If($Quiet) { "-loglevel quiet" }}) -i "$i" -f mp3 "$newname"
+        ffmpeg (&{If($Quiet) { "-loglevel quiet" }}) -i "$Directory/$i" -f mp3 "$Directory/$newname"
     }
 
     Remove-Item $Directory/*.$Type
@@ -95,7 +102,7 @@ if(!(Test-Path -Path $Directory)) {
 # If no url was given, see if we can find one in a meta file
 if($URL -eq "") {
     if(Test-Path -Path $Directory/META.info) {
-        URL=(Get-Content -Path $Directory/META.info)
+        $URL=(Get-Content -Path $Directory/META.info)
     } else {
         if(!$Quiet) {
             Write-Host "Unable to find a META.info file in $Directory. Please create one with a valid URL in it or specify the URL with -URL or -u."
@@ -111,22 +118,23 @@ if($CreateMetaFile) {
     Out-File -FilePath $Directory/META.info -InputObject $URL -Encoding ASCII
 }
 
-if(!$Quiet) {
-    Write-Host "Beginning download."
+if(!$NoDownload) {
+    if(!$Quiet) {
+        Write-Host "Beginning download."
+    }
+    youtube-dl (&{If($Quiet) { "-q" }}) -i -x --download-archive "$Directory/archive.txt" -o "$Directory/%(title)s-v=%(id)s.%(ext)s" "$URL"
 }
 
-youtube-dl (&{If($Quiet) { "-q" }}) -i -x --download-archive "$Directory/archive.txt" -o "$Directory/%(title)s-v=%(id)s.%(ext)s" "$URL"
-
+convert ogg
 convert m4a
 convert opus
 
-# TODO: Uncomment this when testing on Windows
 if(!$NoPlaylist) {
-    $files = Get-ChildItem "$Directory" -Filter *.mp3 *.ogg | Foreach-Object {
-        $OutputPath = Get-Content $_.FullName
-        $content = Split-Path $OutputPath.FullName -leaf
+    if(!$Quiet) {
+        Write-Host "Writing all files to $Directory/playlist.m3u"
     }
+    $files = Get-ChildItem "$Directory" -Filter *.mp3 | Foreach-Object { $_.Name }
 
-    Out-File -FilePath "$Directory/playlist.m3u" -InputObject $files -Encoding ASCII
+    Out-File -FilePath "$Directory/playlist.m3u" -InputObject $files -Encoding UTF8
 }
 
